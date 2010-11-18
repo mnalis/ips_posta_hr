23f33eb2f1f8698bcc8f5b30453b99adfdb988c7
Matija Nalis <mnalis-git@voyager.hr>
2010-11-18 20:53:32 +0100

    init

---
+++ check_ips.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl -wT
+# Matija Nalis <mnalis-perl@voyager.hr> 2010-11-18, GPLv3+
+# prints any changes to packages status
+
+use strict;
+# http://ips.posta.hr/IPSWeb_item_events.asp?itemid=RT073116092HK
